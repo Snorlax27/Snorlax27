8ad8ce9c8c0481c60be6984459aaca63c40d4750
Yazhi Karman <dyazhi@gmail.com>
2017-11-14 15:45:06 -0800

    snorlax init

---
+++ client/app.jsx
@@ -0,0 +1,17 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return(
+      <div>
+      <img src="https://static.comicvine.com/uploads/scale_small/11/114183/5198871-143snorlax.png"></img>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
