f7e8623d4c368a8218270811918c251b74985b7a
Yazhi Karman <dyazhi@gmail.com>
2017-11-14 17:28:57 -0800

    build diary list and entry

--- client/app.jsx
+++ client/app.jsx
@@ -2,13 +2,27 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      entries: []
     }
   }
+
+  componentDidMount() {
+    var scope = this;
+    $.ajax({
+      type: 'GET',
+      url: '/entries',
+      success: function(data) {
+        scope.setState({ entries: data })
+      }
+    })
+  }
+
   render() {
     return(
       <div>
       <img src="https://static.comicvine.com/uploads/scale_small/11/114183/5198871-143snorlax.png"></img>
+
+      <DiaryList list={this.state.entries}/>
       </div>
     )
   }
