b53cc4a781e8b2f33e3ce9334a9f9e8456e70e8b
Yazhi Karman <dyazhi@gmail.com>
2017-11-14 17:31:56 -0800

    build base skeleton

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var mongoClient = require('mongodb').MongoClient;
-mongoose.connect('mongodb://localhost/test', {useMongoClient: true}); //TODO: update test once working
+mongoose.connect('mongodb://localhost/diaries', {useMongoClient: true}); //TODO: update test once working
 console.log('connection successful')
 
 // https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/mongoose
@@ -8,14 +8,14 @@ console.log('connection successful')
 // db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
-var userSchema = mongoose.Schema({
+var UserSchema = mongoose.Schema({
   username: String,
   password: String,
 });
 
 var User = mongoose.model('User', UserSchema);
 
-var diarySchema = mongoose.Schema({
+var DiarySchema = mongoose.Schema({
   username: String,
   text: String,
   sentiment: String,
@@ -33,5 +33,5 @@ var Diary = mongoose.model('Diary', DiarySchema);
 // })
 
 
-module.exports.UserDoc = UserDoc;
-module.exports.DiaryDoc = DiaryDoc;
+module.exports.User = User;
+module.exports.Diary = Diary;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,48 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var port = 8080;
-
+var db = require('../database/db.js');
+// sets up default page
 app.use(express.static(__dirname + '/../public'));
+app.use(bodyParser.json());
 
-// app.get('/', function(req, res) {
-//   res.send('Hello Wuuurld');
+
+app.get('/entries', function(req, res) {
+  db.Diary.find(function(error, data) {
+    if (error) {
+      console.log('error line 12 server.js', error);
+    } else {
+      console.log('success 14 get request', data);
+    }
+    res.send(data);
+  })
+});
+
+app.post('/entries', function(req, res) {
+  addDiaryPost(req.body.text); //calls helper function
+  res.status(200).end();
+});
+
+var addDiaryPost = function(noteText) {
+  var newDiary = new db.Diary({
+    username: noteText,  //THIS WON'T WORK will need to send username with each post request?
+    text: noteText,
+  });
+}
+
+//FAKE DATA:
+// var newDiary = new db.Diary({
+//   username: 'Yazhi the Boss',
+//   text: 'Snorlax fell over ontop of me and I cannot get up. He is soooooooo fatt!!!! :(',
 // });
+
+// newDiary.save(function(err) {
+//   if (err) throw err;
+// });
+
+
 
 app.listen(port, function() {
   console.log('Yayy Server is listening on ' + port);
