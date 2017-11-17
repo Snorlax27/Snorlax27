3fd0d5d3a1a90d282915440410c66d84cf0cffbd
Yazhi Karman <dyazhi@gmail.com>
2017-11-16 19:48:34 -0800

    build login logout, and working on api

--- server/server.js
+++ server/server.js
@@ -118,6 +118,14 @@ var lanuageAPI = function(text) {
 // POST https://language.googleapis.com/v1/documents:analyzeSentiment?key={YOUR_API_KEY}
 
 
+var app = express();
+var port = 8080;
+
+app.use(express.static(__dirname + '/../public'));
+
+// app.get('/', function(req, res) {
+//   res.send('Hello Wuuurld');
+// });
 
 app.listen(port, function() {
   console.log('Yayy Server is listening on ' + port);
