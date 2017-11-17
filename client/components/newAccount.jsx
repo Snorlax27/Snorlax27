106acdfcc26ae6f60d2f364ec0b35584b1577155
Michael MacLean <mrmaclean89@gmail.com>
2017-11-16 19:19:36 -0800

    Updating client only; updated diaryList component. 15 nov 2017

---
+++ client/components/newAccount.jsx
@@ -0,0 +1,49 @@
+class NewAccount extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: "",
+      password: ""
+    }
+    this.handleUsername = this.handleUsername.bind(this);
+    this.handlePassword = this.handlePassword.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: '/newAccount',
+      data: {
+        username: this.state.username,
+        password: this.state.password
+      },
+      success: function() {
+        console.log('line 23 input.jsx post success')
+      }
+    });
+  }
+
+  handleUsername(e) {
+    this.setState({username: e.target.value})
+  }
+
+  handlePassword(e) {
+    this.setState({password: e.target.value})
+  }
+
+
+  render() {
+    return (
+    <div className="login__wrapper">
+      <form onSubmit={this.handleSubmit}>
+        Create Username: <input type="text" placeholder='Enter username' onChange={this.handleUsername}/>
+        Create Password: <input type="text" placeholder='Enter password' onChange={this.handlePassword}/>
+        <button class="btn btn-info" type="submit" onClick={this.handleSubmit}>Make</button>
+      </form>
+    </div>
+    )
+  }
+}
---
+++ public/components/diaryList.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var DiaryList = function DiaryList(_ref) {
+  var list = _ref.list;
+  return React.createElement(
+    "div",
+    null,
+    list.map(function (item) {
+      return React.createElement(
+        "li",
+        null,
+        React.createElement(DiaryEntry, { item: item })
+      );
+    }),
+    ">"
+  );
+};
