901a9fbbb242732d3dc9596ce4206177285db35a
Benji <thebkrcanbefound@gmail.com>
2017-11-22 15:19:12 -0800

    working on front end reflecting sentiment

---
+++ client/components/login.jsx
@@ -0,0 +1,52 @@
+class Login extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: "",
+      password: ""
+    }
+    this.handleUsername = this.handleUsername.bind(this);
+    this.handlePassword = this.handlePassword.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    var scope = this;
+    event.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: '/login',
+      data: {
+        username: this.state.username,
+        password: this.state.password
+      },
+      success: function(data) {
+        console.log('data login.jsx line 24', data)
+        if (data === 'true') {
+          scope.props.handleLogin(scope.state.username);
+        }
+      }
+    });
+  }
+
+  handleUsername(e) {
+    this.setState({username: e.target.value})
+  }
+
+  handlePassword(e) {
+    this.setState({password: e.target.value})
+  }
+
+  render() {
+    return (
+    <div className="loginwrapper" id='signin'>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" placeholder='Enter username' onChange={this.handleUsername}/>
+        <input type="text" placeholder='Enter password' onChange={this.handlePassword}/>
+        <button type="submit" onClick={this.handleSubmit}>Login</button>
+      </form>
+    </div>
+    )
+  }
+}
+
