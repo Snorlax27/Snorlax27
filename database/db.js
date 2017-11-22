e3d9a5fe0829bab5b85ab44877978db9975215b8
Benji <thebkrcanbefound@gmail.com>
2017-11-22 15:17:14 -0800

    database updated for sentiment analysis

--- database/db.js
+++ database/db.js
@@ -1,7 +1,13 @@
 var mongoose = require('mongoose');
 var mongoClient = require('mongodb').MongoClient;
-mongoose.connect('mongodb://localhost/diaries', {useMongoClient: true});
-console.log('connection successful')
+mongoose.connect('mongodb://localhost/diaries', {useMongoClient: true}); //TODO: update test once working
+
+
+// https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/mongoose
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// mongoose.connect('mongodb://localhost/diaries', {useMongoClient: true}); //TODO: update test once working
 
 
 var UserSchema = mongoose.Schema({
@@ -11,15 +17,26 @@ var UserSchema = mongoose.Schema({
 
 var User = mongoose.model('User', UserSchema);
 
+
 var DiarySchema = mongoose.Schema({
   username: String,
   title: String,
-  text: String,
   sentiment: String,
+  text: String
 });
 
 var Diary = mongoose.model('Diary', DiarySchema);
 
 
+
 module.exports.User = User;
 module.exports.Diary = Diary;
+
+// var fluffy = new UserDoc({userName: 'fluffy'});
+// fluffy.save(function (error, success) {
+//   if (error) {
+//     console.log('it didnt work db line 33');
+//   }
+//   console.log('hey Mike it worked :D');
+// })
+
